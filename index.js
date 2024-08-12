e6006637497d527970065ef3bdf8533cdcf165d4
aya.gallarte <aya.gallarte@gmail.com>
2024-08-12 13:07:41 +0800

    modified index.js

--- index.js
+++ index.js
@@ -39,6 +39,51 @@
 // module.exports = {app,mongoose};
 
 
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+// const port = 4000;
+// require("dotenv").config();
+
+// // Routes Middleware
+// const userRoutes = require("./routes/user");
+// const postRoutes = require("./routes/post");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // CORS options
+// const corsOptions = {
+//     origin: [
+//         "https://blog-client-liart.vercel.app",
+//         "https://blog-client-ayas-projects-393c05ff.vercel.app",
+//         "https://blog-client-git-main-ayas-projects-393c05ff.vercel.app"
+//     ],
+//     credentials: true,
+//     optionsSuccessStatus: 200
+// };
+
+// // Use CORS middleware with options
+// app.use(cors(corsOptions));
+
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGODB_STRING);
+
+// mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atlas"));
+
+// // Routes
+// app.use("/posts", postRoutes);
+// app.use("/users", userRoutes);
+
+// // Start server
+// if (require.main === module) {
+//     app.listen(process.env.PORT || port, () => {
+//         console.log
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -78,7 +123,17 @@ mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atl
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 // Start server
 if (require.main === module) {
     app.listen(process.env.PORT || port, () => {
-        console.log
+        console.log(`API is now online on port ${process.env.PORT || port}`);
+    });
+}
+
+module.exports = { app, mongoose };
